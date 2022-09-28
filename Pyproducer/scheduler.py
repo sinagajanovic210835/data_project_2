853a7427a1f973ea1841f363a2414fdb6eba947f
Sinisa Gajanovic <sinagajanovic@gmail.com>
2022-09-28 15:18:32 +0200

    some changes

--- Pyproducer/scheduler.py
+++ Pyproducer/scheduler.py
@@ -1,30 +1,50 @@
 from time import time, ctime, struct_time
 import calendar
 
+cntrFile = open("../Csv/country/country.csv", "r")
+cntrFile.close()
+prodFile = open("../Csv/country/country.csv", "r")
+prodFile.close()
+countriesWithCode = {}
+
+with open("../Csv/country/country.csv") as file:
+    for line in file:
+        if line.strip():
+            (key, val) = line.strip().split(";")
+            countriesWithCode[key] = val
+    file.close()
+
+
+
 cnt = 0
 header = True
 start = 0
 first = True
-
+countries = set()
+products = set()
 newFile = open("../Csv/country/country.csv", "r")
+newFile.close()
+
 with open("../Csv/invoices/invoices.csv") as invoices:
     for line in invoices:
         if line.strip() and not header:
             arr = line.strip().split(";")            
-            dt = arr[len(arr) - 3].split(" ")[0]
-            tm = arr[len(arr) - 3].split(" ")[1]
+            dt = arr[-3].split(" ")[0]
+            tm = arr[-3].split(" ")[1]
             day = int(dt.split("/")[1])
             mnth = int(dt.split("/")[0])
             yr = int(dt.split("/")[2])
             hr = int(tm.split(":")[0])
             minut = int(tm.split(":")[1])
+            countries.add(arr[-1], countriesWithCode[arr[-1]])
             if first:
+                startDay = day
                 cnt += 1
                 newFile.close()
                 start = hr
                 name = "../Csv/invoicesByHour/" + str(cnt) + "--" + str(start) + ":00-" + str(start + 1) + ":00--" + str(day)  + "-" + str(mnth) + "-" + str(yr) + ".csv"
                 newFile = open(name, "w")
-                newFile.write("InvoiceNo,StockCode,Description,Quantity,InvoiceDate,UnitPrice,CustomerID,Country")
+                newFile.write("InvoiceNo;StockCode;Quantity;InvoiceDate;CustomerID;Country\n")
                 first = False
             if hr == start:
                 newFile.write(line.strip() + "\n")
@@ -33,6 +53,7 @@ with open("../Csv/invoices/invoices.csv") as invoices:
                 newFile.close()
         header = False
     invoices.close()
+
 # print("**************************************")
 # t = time()
 # print(t)
